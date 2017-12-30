d885be3146c147cffed04c3021d6d6471fbb9772
Rekha-Lanka <l.s.rekha.29@gmail.com>
2017-12-30 14:18:54 +0530

    survey second commit

--- app/src/main/java/com/delhijal/survey/NewSurvey/PersonDetailsActivity.java
+++ app/src/main/java/com/delhijal/survey/NewSurvey/PersonDetailsActivity.java
@@ -3,12 +3,14 @@ package com.delhijal.survey.NewSurvey;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.delhijal.survey.MainSurveyActivity;
 import com.delhijal.survey.R;
 
-public class PersonDetailsActivity extends Activity {
+public class PersonDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,12 @@ public class PersonDetailsActivity extends Activity {
             }
         });
 
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+//        onBackPressed();
+        Intent i=new Intent(PersonDetailsActivity.this,MainSurveyActivity.class);
+        startActivity(i);
+       return true;
     }
 }
--- app/src/main/java/com/delhijal/survey/NewSurvey/SubmitDetailsActivity.java
+++ app/src/main/java/com/delhijal/survey/NewSurvey/SubmitDetailsActivity.java
@@ -1,15 +1,35 @@
 package com.delhijal.survey.NewSurvey;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.delhijal.survey.R;
 
 public class SubmitDetailsActivity extends Activity {
-
+ Button sprevious,submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_details);
+        sprevious=(Button)findViewById(R.id.sprevious);
+        submit=(Button)findViewById(R.id.submit);
+        sprevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(SubmitDetailsActivity.this,ConnectionDetailsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent i=new Intent(SubmitDetailsActivity.this,OwnerDetailsActivity.class);
+//                startActivity(i);
+            }
+        });
     }
 }
